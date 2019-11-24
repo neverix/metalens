6da3959c4868023148f3f0709852766d4083c970
ax37 <ax37@tuta.io>
2019-11-25 00:30:39 +0300

    Basic skeleton for rendering.

--- src/raytracer.ts
+++ src/raytracer.ts
@@ -8,6 +8,11 @@ export default class Raytracer {
     }
 
     render(image: ImageData) {
-
+        for (let x = 0; x < image.width; x++) {
+            for (let y = 0; y < image.height; y++) {
+                const ray = this.camera.castRay(x, y)
+                // TODO
+            }
+        }
     }
 }
