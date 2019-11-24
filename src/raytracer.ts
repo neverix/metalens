1e75d7a837d60d7e37e34b110813443f03bc5a2d
ax37 <ax37@tuta.io>
2019-11-25 01:08:08 +0300

    Implemented ray tracing.

--- src/raytracer.ts
+++ src/raytracer.ts
@@ -1,20 +1,41 @@
 import Camera from "./camera"
 import { Surface } from "./surface"
+import { vec3 } from "gl-matrix"
 
 export default class Raytracer {
     camera: Camera
     world: Surface
+    bounces: number
 
-    constructor(camera: Camera, world: Surface) {
+    constructor(camera: Camera, world: Surface, bounces?: number) {
         this.camera = camera
         this.world = world
+        this.bounces = bounces || 3
     }
 
     render(image: ImageData) {
+        /**
+         * Renders and image using raycasting
+         */
+        // Loop over all pixels
         for (let x = 0; x < image.width; x++) {
             for (let y = 0; y < image.height; y++) {
-                const ray = this.camera.castRay(x, y)
-                // TODO
+                // Cast ray from camera surface
+                let ray = this.camera.castRay(x, y)
+                // Default color is white
+                let color = vec3.fromValues(255, 255, 255)
+                for (let i = 0; i < this.bounces; i++) {
+                    // Do hit test
+                    const hit = this.world.hit(ray)
+                    if (hit == null) break
+                    ray = hit.ray
+                    // Modulate color
+                    vec3.mul(color, color, hit.albedo)
+                }
+                // Set image pixel RGBA values
+                const startIndex = (image.width * y + x) * 4
+                image.data.set(color, startIndex)
+                image.data[startIndex + 3] = 255
             }
         }
     }
